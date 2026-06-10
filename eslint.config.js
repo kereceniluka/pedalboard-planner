4a495129291f107419bb97d2d8efcd5b916a7326
Luka Kereceni <luka.kereceni@gmail.com>
2026-06-10 14:05:21 +0200

    Add eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,9 +16,15 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'warn',
+      'react/prop-types': 'off',
+    },
     languageOptions: {
       globals: globals.browser,
     },
   },
-])
+]);
