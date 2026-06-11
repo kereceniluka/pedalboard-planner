8bf0b9c9f7e5c53168919200f5d3d79eea8f9efc
Luka Kereceni <luka.kereceni@gmail.com>
2026-06-11 18:19:27 +0200

    test: vercel auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,6 @@ function App() {
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
         <button
           type="button"
           className="counter"
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"; // 1. Import the Node.js path module
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
@@ -5,4 +6,9 @@ import babel from '@rolldown/plugin-babel';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 2. Map the "@" alias to your "/src" directory
+    },
+  },
 });
